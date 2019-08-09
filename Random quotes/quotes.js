602527f65374707e968e04ae18e2513c7ee2cbda
Sayantan <sayantandatta97@gmail.com>
2019-08-09 23:03:43 +0530

    added files

---
+++ Random quotes/quotes.js
@@ -0,0 +1,18 @@
+var quotes = ['Sun','is','star','planet','Life','a','war','game','lights'];
+
+var author = ['Ram Sukla', 'Krishna Gupta','Joy Roy','Sam Patro'];
+
+newQuote(quotes,author);
+
+dsocument.getElementById('nxtqt').click(function(){
+    newQuote(quotes,author);
+    
+});
+
+function newQuote(q,a){
+    qtNum = Math.floor(Math.random() * quotes.length);
+    authNum = Math.floor(Math.random() * author.length);
+    document.getElementById('qt').text('" '+ q[qtNum] + ' "');
+    dsocument.getElementById('auth').text('" '+ a[authNum] + ' "');
+
+}
