9f2971f7d2998e8689b12578bb428756592f1f75
Sayantan <sayantandatta97@gmail.com>
2019-08-10 21:00:30 +0530

    Update quotes.js

--- Random quotes/quotes.js
+++ Random quotes/quotes.js
@@ -1,3 +1,4 @@
+/*
 var quotes = ['Sun','is','star','planet','Life','a','war','game','lights'];
 
 var author = ['Ram Sukla', 'Krishna Gupta','Joy Roy','Sam Patro'];
@@ -14,5 +15,39 @@ function newQuote(q,a){
     authNum = Math.floor(Math.random() * author.length);
     document.getElementById('qt').text('" '+ q[qtNum] + ' "');
     dsocument.getElementById('auth').text('" '+ a[authNum] + ' "');
+
+}
+*/
+
+var quotes = ['Sun','is','star','planet','Life','a','war','game','lights'];
+
+var author = ['Ram Sukla', 'Krishna Gupta','Joy Roy','Sam Patro'];
+
+newQuote(quotes,author);
+
+dsocument.getElementById('nxtqt').click(function(){
+    newQuote(quotes,author);
+    
+});
+
+function newQuote(q,a){
+    //qtNum = Math.floor(Math.random() * quotes.length);
+    authNum = Math.floor(Math.random() * author.length);
+    var n=4;
+    var quote=[];
+
+    for (var i =0; i<=n; i++){
+        qtNum = Math.floor(Math.random() * quotes.length);
+        quote.push(q[qtNum]);
+        console.log(q[qtNum]);
+      }
+    
+    var newQ = quote.slice(0, quote.length).join(' ');
+      console.log(newQ);
+    
+
+    return(alert(newQ)); 
+    document.getElementById('qt').text(newQ);
+    document.getElementById('auth').text( a[authNum] );
 
 }
